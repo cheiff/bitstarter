1a2a503dabca5695594b81fc57eeabf2ae5d4737
Cheiff <javi@jpo.cc>
2013-07-11 22:47:42 +0200

    Finished function that allows loading from url

--- grader.js
+++ grader.js
@@ -26,12 +26,20 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
 var rest = require('restler');
  
 var getUrl = function(url){
-    rest.get(url).on('complete',function(response){
-	console.log(response);
-    });
+    if (url!=''){
+	rest.get(url).on('complete',function(response){
+             fs.writeFileSync(HTMLFILE_DEFAULT, response);
+	    //return(response);
+	    //console.log(response);
+	});
+    } else {
+	console.log('Url is empty. Exiting.');
+	process.exit(1);
+    }
 }
 
 var assertFileExists = function(infile) {
@@ -72,8 +80,13 @@ if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-u, --url <url>', 'Url to index.html', getUrl(myurl), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url to index.html')
         .parse(process.argv);
+console.log('Echoing '+program.url);
+if (typeof program.url != 'undefined'){
+    var downloadedSite = getUrl(program.url);
+}
+
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
